5aba712bbd28853d41c0f787fe3a48b8906702d5
NutCrazypanda <56244402+NutCrazypanda@users.noreply.github.com>
2021-03-13 18:41:42 +0700

    Update largefile.py

--- largefile.py
+++ largefile.py
@@ -4,7 +4,7 @@ print("Select option")
 print("1.finding by file size")
 print("2.finding by file name")
 print("Please select : ")
-menu_select = str(input())
+menu_select = int(input())
 print(menu_select)
 # folder path input
 print("Enter folder path:")
